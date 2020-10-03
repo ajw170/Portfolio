384262d3b1dd9efa460e49043a391f0a08703e26
Andrew Wood <wood.andrew9@gmail.com>
2020-10-03 10:48:53 -0400

    Final commit.

--- jobs/models.py
+++ jobs/models.py
@@ -4,4 +4,8 @@ from django.db import models
 class Job(models.Model):
     image = models.ImageField(upload_to='images/')
     summary = models.CharField(max_length=200)
+    short_summary = models.CharField(max_length=50,default='Add Summary Here',name='Short Summary')
 
+
+    def __str__(self):
+        return self.summary
